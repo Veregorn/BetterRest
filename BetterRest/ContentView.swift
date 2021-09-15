342193c16a9a22befe3a3ed9ef8a8b164dd64cdd
Raul Jimenez <rjbarco@gmail.com>
2021-09-15 23:15:04 +0200

    Added Stepper(step) and changed the precision for the Text View

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,8 +11,8 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4...12) {
-            Text("\(sleepAmount) hours")
+        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+            Text("\(sleepAmount, specifier: "%.2f") hours")
         }
     }
 }
